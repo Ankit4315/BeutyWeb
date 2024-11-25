a976b2a6c93e6e308b95b22ed210489a019892bc
ankit dhakad <varmaankit0996@gmail.com>
2024-11-25 22:51:30 +0530

    sec

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <footer> 
+        <div>Footer</div>
+    </footer>
+    
+  )
+}
+
+export default Footer
---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <>
+      <header>
+        <div>Navbar</div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
